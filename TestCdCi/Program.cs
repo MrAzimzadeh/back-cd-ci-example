527c2f19d032fad53a57506c429a38fe5686e61c
MrAzimzadeh <mehemmedezimzase@gmail.com>
2025-04-09 16:51:46 +0400

    add

--- TestCdCi/Program.cs
+++ TestCdCi/Program.cs
@@ -1,11 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(8080);
+});
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
